885cff3cdeff05f93d735fa1faa5c867daee110c
Junsu Park <pksjmh5295@gmail.com>
2023-07-10 16:58:28 +0900

    [ADD] Spring Security

--- src/main/java/com/practice/reverselevel/spring/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/practice/reverselevel/spring/config/auth/dto/OAuthAttributes.java
@@ -33,7 +33,42 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registeredId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registeredId)){
+            return ofNaver("id", attributes);
+        }
+        if("kakao".equals(registeredId)){
+            return ofKakao("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName,
+                                           Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName,
+                                           Map<String, Object> attributes) {
+        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        return OAuthAttributes.builder()
+                .name((String) profile.get("nickname"))
+                .email((String) kakaoAccount.get("email"))
+                .picture((String) properties.get("profile_image"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
--- src/main/java/com/practice/reverselevel/spring/web/IndexController.java
+++ src/main/java/com/practice/reverselevel/spring/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.practice.reverselevel.spring.web;
 
+import com.practice.reverselevel.spring.config.auth.LoginUser;
 import com.practice.reverselevel.spring.config.auth.dto.SessionUser;
 import com.practice.reverselevel.spring.service.posts.PostsService;
 import com.practice.reverselevel.spring.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
